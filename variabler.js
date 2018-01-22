d3eff157234bdf530050f212eef7173f9e072377
Tamara <4744385@rts-365.dk>
2018-01-22 09:38:19 +0100

    com1

---
+++ variabler.js
@@ -0,0 +1,16 @@
+var name = "Tamara";
+var alder = "1";
+
+let byNavn = "Roskilde";
+console.log(byNavn);
+
+var x = 5;
+var y = 10; 
+var total = x + y;
+
+var x = 5;
+var y = 10; 
+var z = x + y; 
+
+console.log('Hej, jeg hedder ' + mitFornavn + ' ' + mitEfternavn+ ' og jeg er ' + minAlder + ' år');
+
