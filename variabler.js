e986c70ec0ef03064ac06c0da47ddb8568292914
Tamara <4744385@rts-365.dk>
2018-01-22 10:26:28 +0100

    com2

--- variabler.js
+++ variabler.js
@@ -7,10 +7,26 @@ console.log(byNavn);
 var x = 5;
 var y = 10; 
 var total = x + y;
+console.log(total);
 
 var x = 5;
 var y = 10; 
 var z = x + y; 
+console.log(z);
 
-console.log('Hej, jeg hedder ' + mitFornavn + ' ' + mitEfternavn+ ' og jeg er ' + minAlder + ' år');
+var navn = "Tamara";
+var alder = "1";
+var efternavn = "Fanayei";
+console.log("Hej, jeg hedder"+" "  + navn+" "  + efternavn + "og jeg er" + alder + "år");
+
+
+var moms = 1.25;
+var prisUdenMoms = 400; 
+var prisMedMoms = moms * prisUdenMoms;
+var prisUdenMoms2 = prisMedMoms / moms
+console.log(prisMedMoms);
+console.log(prisUdenMoms2);
+
+forste = 10; 
+anden = 20;
 
