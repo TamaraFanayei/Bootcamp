c91d5b2c3683d628989d0bf6a91df2de8118a60e
Tamara <4744385@rts-365.dk>
2018-01-23 12:33:41 +0100

    com1

--- loekker.js
+++ loekker.js
@@ -1,64 +1,64 @@
-// for(let i = 0; i < 26; i++){
-//     console.log(i);
-// }
+for (let i = 0; i < 26; i++) {
+    console.log(i);
+}
 
-// let x = 1;
+let x = 1;
 
-// while(x < 26){
-// console.log(x, "Hej Verden");
-// x++; 
-// }
-
-
-// let x = 1;
-
-// while(x < 25){
-// console.log(x, "Hej Verden");
-// x++; 
-// }
-
-// let x;
-
-// do{
-// x = 25;
-// console.log(x, "hej");
-// }
+while (x < 26) {
+    console.log(x, "Hej Verden");
+    x++;
+}
 
 
-// for(let i = 1917; i < 2018; i++){
-//     console.log(i);
-// }
+let x = 1;
 
-// let dage = ["mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag", "søndag"];
+while (x < 25) {
+    console.log(x, "Hej Verden");
+    x++;
+}
 
-// dage.forEach(function(dage, index){
-//     console.log(dage[index]);
-//     console.log(dage + " er placeret på " + index + " plads");
-// });
+let x;
 
-// let navne = ["Simon", "Anne", "Bo", "Ib", "Jo", "Le", "Hans"];
-
-// navne.forEach(function(navne, index){
-//     console.log(navne);
-// });
+do {
+    x = 25;
+    console.log(x, "hej");
+} while (x < 25)
 
 
-// let navne = ["Simon", "Anne", "Bo", "Ib"];
+for (let i = 2018; i < 1917; i--) {
+    console.log(i);
+}
 
-// navne.forEach(function(navne, index){
-//     console.log(navne [index]);
-//     console.log(navne + " er placeret på " + index + " plads");
-// });
+let dage = ["mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag", "søndag"];
+
+dage.forEach(function (dage, index) {
+    console.log(dage[index]);
+    console.log(dage + " er placeret på " + index + " plads");
+});
+
+let navne = ["Simon", "Anne", "Bo", "Ib", "Jo", "Le", "Hans"];
+
+navne.forEach(function (navne, index) {
+    console.log(navne);
+});
+
+
+let navne = ["Simon", "Anne", "Bo", "Ib"];
+
+navne.forEach(function (navne, index) {
+    console.log(navne[index]);
+    console.log(navne + " er placeret på " + index + " plads");
+});
 
 let alletal = [4, 6, 10, 8, 10, 14, 20, 16, 18, 2];
 
-alletal.forEach(function(tal, index){
-    console.log(tal < alletal [index+1])
-if(tal < alletal [index+1]){
-console.log(tal + "er mindre end" + alletal [index+1])
-}
-else(tal + "er større end" + alletal [index-1]
-)
+alletal.forEach(function (tal, index) {
+    console.log(tal < alletal[index + 1])
+    if (tal < alletal[index + 1]) {
+        console.log(tal + "er mindre end" + alletal[index + 1])
+    }
+    else (tal + "er større end" + alletal[index - 1]
+    )
 
 });
 
