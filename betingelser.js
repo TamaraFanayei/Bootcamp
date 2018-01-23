95d7a2ea17167be0495760fd2792197d146f1386
Tamara <4744385@rts-365.dk>
2018-01-23 11:13:14 +0100

    1

---
+++ betingelser.js
@@ -0,0 +1,44 @@
+if (true) {
+    Hvis true - udføres koden
+}
+else {
+    Hvis ovenstående er falsk - udfør denne kode
+}
+
+else if () {
+    Og ellers udfør denne kode. 
+}
+
+let x = true;
+if (x) {
+    console.log("Hej verden");
+}
+
+let x = 8;
+console.log(x);
+
+if (x > 5) {
+    console.log(x + "er større end 5")
+}
+else {
+    console.log(x + "er mindre end 5")
+}
+
+
+let number = 8;
+console.log(number);
+
+console.log((number < 5 ? "IF" : "ELSE")) //en ternary operator = en kort måde at lave if og else. If og else er på hver side af : 
+
+
+var drik = "juice";
+console.log(drik);
+if (drik == "mælk") {
+    "test1"
+}
+else if (drik == "cola") {
+    "test2"
+}
+else {
+    "ukendt"
+}
