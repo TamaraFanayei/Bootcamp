13f570ccda057cfa19cb56442ec7e6353c4de15f
Tamara <4744385@rts-365.dk>
2018-01-23 14:24:31 +0100

    com1

--- Operatorer.js
+++ Operatorer.js
@@ -2,6 +2,53 @@ Operator.js
 
 let x = 3;
 let y = 6;
+x + y;
+console.log(x);
+
+let x = 3;
+let y = 6;
+x - y;
+console.log(x);
+
+let x = 3;
+let y = 6;
+x * y;
+console.log(x);
+
+let x = 3;
+let y = 6;
+x / y;
+console.log(x);
+
+let x = 3;
+let y = 6;
+x++ y;
+console.log(x);
+
+let x = 3;
+let y = 6;
+x % y;
+console.log(x);
+
+let x = 3;
+let y = 6;
+x-- y;
+console.log(x);
+
+let x = 3;
+let y = 6;
+x += y;
+console.log(x);
 
 
+
+let x = 3;
+let y = 6;
+x == y;
+console.log(x);
+
+let x = 3;
+let y = 6;
+x != y;
+console.log(x);
 
