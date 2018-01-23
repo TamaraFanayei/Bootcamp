7f059b5e8712f5a798d281307be764f77154fc3a
Tamara <4744385@rts-365.dk>
2018-01-23 10:33:36 +0100

    com5

--- arrays.js
+++ arrays.js
@@ -1,2 +1,23 @@
-arrays.js
-Se arrays under loekker dokument. 
+// let frugter = ["Pære", "Æble", "og", "Banan"];
+// console.log(frugter);
+// console.log(frugter[1]);
+
+//frutger[3] = fersken;
+//console.log(frugter);
+
+// let frugter = ["Pære", "Æble", "og", "Banan", "Fersken"];
+// console.log(frugter);
+
+// let frugter = ["Pære", "Æble", "og", "Banan", "Fersken"];
+// console.log(frugter.length);
+
+// let frugter = ["Pære","Æble","og","Banan","Fersken","appelsin"];
+// console.log(frugter.length); Hvad menes der med denne opgave? 
+
+let navne = ["Simon", "Anne", "Bo"];
+let efternavn = ["Simonsen", "Annesen", "Bosen"];
+let alder = ["13", "14", "15"];
+console.log(navne + efternavn + alder);
+
+
+
