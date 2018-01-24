fb36a548bbc216132fc273ac9d893e169683446c
Tamara <4744385@rts-365.dk>
2018-01-24 14:16:11 +0100

    com2

---
+++ hyggeopgaver.js
@@ -0,0 +1,51 @@
+// Fix koden 1: Fejlbeskeder tekststreng
+// Problem:
+// Når den udleverede kode er kørt, indeholder variablen fejlbeskeder kun den sidste fejlbesked, "Du skal udfylde adresse feltet".
+
+// Gør det, der skal til, for at alle beskederne bliver lagt ned i variablen fejlbeskeder.
+
+// // Disse oplysninger kommer selvfølgelig typisk fra en formular
+
+// var navn = "";
+// var alder = "";
+// var adresse = "";
+
+// var fejlbeskeder = "";
+
+// if (navn == "") {
+//     fejlbeskeder = "Du skal udfylde navn feltet\n";
+// }
+
+// if (alder == "") {
+//     fejlbeskeder = "Du skal udfylde alder feltet\n";
+// }
+
+// if (adresse == "") {
+//     fejlbeskeder("Du skal udfylde adresse feltet\n");
+// }
+// console.log(fejlbeskeder);
+
+
+// if (document.formular.navn.value == "") {
+//     alert("dit navn er ikke udfyldt");
+//     return false;
+// }
+
+// Palindromer
+// Skriv en funktion som tager imod en tekststreng
+// og returnerer true eller false afhængigt af om tekststrengen er et palindrom.
+
+var palindrom = "stets";
+console.log(drik);
+if (palindrom == "mælk") {
+    "test1"
+}
+else if (palindrom == "cola") {
+    "test2"
+}
+else {
+    "ukendt"
+}
+console.log(palidrom);
+
+
