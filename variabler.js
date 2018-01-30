19a0501b80d212401625082209719059c3d285e0
Tamara <4744385@rts-365.dk>
2018-01-30 08:24:01 +0100

    com1

--- variabler.js
+++ variabler.js
@@ -5,30 +5,31 @@ let byNavn = "Roskilde";
 console.log(byNavn);
 
 var x = 5;
-var y = 10; 
+var y = 10;
 var total = x + y;
 console.log(total);
 
 var x = 5;
-var y = 10; 
-var z = x + y; 
+var y = 10;
+var z = x + y;
 console.log(z);
 
 var navn = "Tamara";
 var alder = "1";
 var efternavn = "Fanayei";
-console.log("Hej, jeg hedder "+ navn+" "  + efternavn + " og jeg er " + alder + "år");
+console.log("Hej, jeg hedder " + navn + " " + efternavn + " og jeg er " + alder + "år");
 
 
 var moms = 1.25;
-var prisUdenMoms = 400; 
+var prisUdenMoms = 400;
 var prisMedMoms = moms * prisUdenMoms;
 var prisUdenMoms2 = prisMedMoms / moms
 console.log(prisMedMoms);
 console.log(prisUdenMoms2);
 
-forste = 10; 
+forste = 10;
 anden = 20;
+
 
 
 
